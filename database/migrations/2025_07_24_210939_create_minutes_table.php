8b4c8054e60a163601ef08c0825b9b9f7959314c
hanine248 <haninetarhini2003@gmail.com>
2025-07-25 01:20:18 +0300

    databasemigrtion

---
+++ database/migrations/2025_07_24_210939_create_minutes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('minutes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('meeting_id')->constrained('meetings')->onDelete('cascade');
+            $table->enum('status', ['draft', 'submitted', 'approved'])->default('draft');
+            $table->text('decision')->nullable();
+            $table->text('summary')->nullable();
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('minutes');
+    }
+};
